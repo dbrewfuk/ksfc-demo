bd00bf3e983b31d3d7464349bb13b442e9ebfcc8
Daniel <dbrewfukuhara@gmail.com>
2023-02-01 15:10:27 -0500

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const programs = [
+  {
+    id: 1,
+    college: "College A",
+    program: "Computer Science",
+    academicArea: "Science & Technology",
+    contactPerson: "Jane Doe",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "janedoe@email.com"
+  },
+  {
+    id: 2,
+    college: "College B",
+    program: "Business Administration",
+    academicArea: "Business & Economics",
+    contactPerson: "John Smith",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "johnsmith@email.com"
+  },
+  {
+    id: 3,
+    college: "College C",
+    program: "English Literature",
+    academicArea: "Arts & Humanities",
+    contactPerson: "Sarah Johnson",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "sarahjohnson@email.com"
+  },
+  {
+    id: 4,
+    college: "College D",
+    program: "Chemistry",
+    academicArea: "Science & Technology",
+    contactPerson: "David Smith",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "davidsmith@email.com"
+  },
+  {
+    id: 5,
+    college: "College E",
+    program: "Marketing",
+    academicArea: "Business & Economics",
+    contactPerson: "Emily Davis",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "emilydavis@email.com"
+  },
+  {
+    id: 6,
+    college: "College F",
+    program: "History",
+    academicArea: "Arts & Humanities",
+    contactPerson: "Michael Johnson",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "michaeljohnson@email.com"
+  },
+  {
+    id: 7,
+    college: "College G",
+    program: "Biology",
+    academicArea: "Science & Technology",
+    contactPerson: "Jessica Lee",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "jessicalee@email.com"
+  },
+  {
+    id: 8,
+    college: "College H",
+    program: "Accounting",
+    academicArea: "Business & Economics",
+    contactPerson: "William Brown",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "williambrown@email.com"
+  },
+  {
+    id: 9,
+    college: "College I",
+    program: "Art History",
+    academicArea: "Arts & Humanities",
+    contactPerson: "Karen Wilson",
+    contactTitle: "Title",
+    contactPhoneNumber: "(012) 234 - 5678",
+    contactEmail: "karenwilson@email.com"
+  }
+]
