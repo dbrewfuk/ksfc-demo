55ca5df919b68f30a41b10bc5c2fe2db67dd067c
dbrewfuk <dbrewfuk@users.noreply.github.com>
2023-10-04 17:03:38 +0000

    feat: Update vite.config.js
    Added base path, outDir, assetsDir, and server settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
 });
