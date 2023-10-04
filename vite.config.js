28769187526fe847cb49b5bac497d08f915ed1b1
dbrewfuk <dbrewfuk@users.noreply.github.com>
2023-10-04 17:25:02 +0000

    feat: Update vite.config.js
    Add new pages and update existing page paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path"; 
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -12,7 +12,12 @@ export default defineConfig({
     assetsDir: "assets",
     rollupOptions: {
       input: {
-        interior: resolve(__dirname, "interior-landing.html"),
+        home: resolve(__dirname, "home.html"),
+        interior: resolve(__dirname, "interior.html"),
+        interiorLanding: resolve(__dirname, "interior-landing.html"),
+        resources: resolve(__dirname, "resources.html"),
+        rescueTraining: resolve(__dirname, "rescue-training.html"),
+        aboutUs: resolve(__dirname, "about-us.html"),
       },
     },
   },
